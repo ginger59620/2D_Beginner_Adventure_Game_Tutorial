745aee50a6678e1fc37120ac805411b8b7f84e17
ginger59620 <a59620@alunos.ipb.pt>
2025-03-09 17:17:44 +0000

    Player Character and Movement

---
+++ Assets/SCRIPTS/PLAYERCONTROLLER.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PLAYERCONTROLLER : MonoBehaviour
+{
+    public InputAction MoveAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveAction.Enable(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
+        transform.position = position; ;
+    }
+}
